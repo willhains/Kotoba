eb5ee932a8434a0d6af5ace19d8022892e978e4e
Will Hains <github@willhains.com>
2014-12-07 12:32:45 +0900

    Custom search bar appearance.
    Larger font; taller search bar.

--- code/Dictionary/DictionaryViewController.swift
+++ code/Dictionary/DictionaryViewController.swift
@@ -12,9 +12,21 @@ class DictionaryViewController: UIViewController, UISearchBarDelegate
 {
 	@IBOutlet weak var searchBar: UISearchBar!
 	@IBOutlet weak var dictionaryContainer: UIView!
-	
+
 	override func viewDidAppear(animated: Bool)
 	{
+		// Increase size of font and height of search bar
+		let helveticaNeueBig = UIFont(name: "Helvetica Neue", size: 24)
+		for subview in findAllSubviews(self.searchBar)
+		{
+			if subview is UITextField
+			{
+				let textField = subview as UITextField
+				textField.font = helveticaNeueBig
+				textField.bounds.size.height = 88
+			}
+		}
+		
 		// Show the keyboard on launch, so you can start typing right away
 		self.searchBar.becomeFirstResponder()
 	}
@@ -45,4 +57,16 @@ class DictionaryViewController: UIViewController, UISearchBarDelegate
 		// Hide keyboard
 		searchBar.resignFirstResponder()
 	}
+}
+
+// Cheater's function to rummage through the subviews of the given UIView
+func findAllSubviews(view: UIView) -> [UIView]
+{
+	var subviews = [UIView]()
+	for subview in view.subviews
+	{
+		subviews.append(subview as UIView)
+		subviews.extend(findAllSubviews(subview as UIView))
+	}
+	return subviews
 }
