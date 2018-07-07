72a0519ef7804c3b1260f2b76b9d319e832ba659
Will Hains <will@willhains.com>
2018-07-07 21:49:34 +0900

    Fix misaligned input field.

--- code/Kotoba/Workarounds.swift
+++ code/Kotoba/Workarounds.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-extension NSDictionary {
-	
+extension NSDictionary
+{
 	// iOS 12 beta apparently has a crash (exception) where UIDictionaryManager tries to access
 	// a property via _isTTYEnabled that it expects to be a value (String or Number), probably
 	// from some dictionary somewhere, that it can turn into a Bool by calling boolValue, but on
@@ -17,8 +17,8 @@ extension NSDictionary {
 	// by implementing that method in an extension of NSDictionary to always return false.  This
 	// should be removed in the future if it's not needed because the underlying code is fixed.
 
-	@objc func boolValue() -> Bool {
+	@objc func boolValue() -> Bool
+    {
 		return false
 	}
-	
 }
