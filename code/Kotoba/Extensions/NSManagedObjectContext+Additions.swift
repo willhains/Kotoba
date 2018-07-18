772262c947f48e68a2fa5a81d99e7e6af5208d9d
Gabor Halasz <gaborthefisher@protonmail.com>
2018-07-18 13:31:55 +0200

    adding managed object context extension

---
+++ code/Kotoba/Extensions/NSManagedObjectContext+Additions.swift
@@ -0,0 +1,37 @@
+//
+//  NSManagedObjectContext+Additions.swift
+//  Kotoba
+//
+//  Created by Gabor Halasz on 18/07/2018.
+//  Copyright © 2018 Will Hains. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension NSManagedObjectContext {
+  func makeChanges(_ changes: @escaping () -> Void) {
+    perform { [weak self] in
+      changes()
+      _ = self?.saveOrRollBack()
+    }
+  }
+  
+  private func saveOrRollBack() -> Bool {
+    do {
+      try save()
+      return true
+    } catch {
+      rollback()
+      return false
+    }
+  }
+}
+
+extension NSManagedObjectContext {
+  func insertObject<A: NSManagedObject>() -> A where A: Managed {
+    guard let object = NSEntityDescription.insertNewObject(forEntityName: A.entityName, into: self) as? A
+      else { fatalError("Wrong object type") }
+    return object
+  }
+}
