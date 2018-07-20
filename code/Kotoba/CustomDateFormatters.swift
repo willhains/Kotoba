5b4a2b945b37b52a3a84956eb99dad43d65c38c5
Gabor Halasz <gaborthefisher@protonmail.com>
2018-07-20 12:21:45 +0200

    Adding class for holding date formatter. Moving story board file

---
+++ code/Kotoba/CustomDateFormatters.swift
@@ -0,0 +1,21 @@
+//
+//  DateFormatters.swift
+//  Kotoba
+//
+//  Created by Gabor Halasz on 20/07/2018.
+//  Copyright © 2018 Will Hains. All rights reserved.
+//
+
+import Foundation
+
+final class CustomDateFormatters {
+  static let shared: CustomDateFormatters = CustomDateFormatters()
+  let wordQueryDateFormatter: DateFormatter
+  
+  init() {
+    wordQueryDateFormatter = DateFormatter()
+    wordQueryDateFormatter.calendar = Calendar.current
+    wordQueryDateFormatter.locale = Locale.current
+    wordQueryDateFormatter.dateStyle = .medium
+  }
+}
