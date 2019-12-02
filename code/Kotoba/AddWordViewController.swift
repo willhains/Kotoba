66c7fa643e8da6d5e7d734270cbdd00d291e7da0
Will Hains <will@willhains.com>
2019-12-03 00:07:20 +0900

    Drop landscape support?
    I generally don't like using my iPhone in landscape, except for video and photos. iPad is a different matter, of course.

--- code/Kotoba/AddWordViewController.swift
+++ code/Kotoba/AddWordViewController.swift
@@ -302,6 +302,7 @@ extension AddWordViewController
 		// NOTE: This isn't the most elegant way to handle the compact height layout (landscape orientation)
 		// but it gets the job done with a minimum amount of work. I'm actually questioning the need for
 		// landscape support: the entire experience feels kinda clunky.
+		// CHOCK: I'd be happy to drop landscape support, tbh.
 		let maximumHeight: CGFloat = view.frame.size.height > view.frame.size.width ? 200 : 100
 		
 		var computedSuggestionHeight = suggestionHeaderHeight
